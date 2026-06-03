757794f593e391f8573d083629ae71a82088d314
szh3543304389@163.com <szh3543304389@163.com>
2026-06-03 10:30:53 +0800

    v2.6.1a 移除全局RollArray头文件并将其移入WindowManager

--- include/engine/gui/MyGUI.hpp
+++ include/engine/gui/MyGUI.hpp
@@ -5,7 +5,6 @@
 #include "engine/serialization/JsonExtensions.hpp"
 #include "engine/resource/ResourceManager.hpp"
 #include "engine/data/VarianTmap.hpp"
-#include "engine/data/RollArray.hpp"
 //if exist then check condition
 #define ensure(existCondition,condition) (!(existCondition)||((existCondition)&&(condition)))
 namespace gui {
@@ -888,6 +887,42 @@ namespace gui {
 		bool isDragScrolling = false;
 		bool mousePressed = false;
 		unsigned int cursorBlinkTick = 0;
+		template<typename T, int capacity>
+		class RollArray {
+		private:
+			T array[capacity];
+			size_t realSize = 0;
+			size_t backPosition = 0;
+		public:
+			inline void emplace_back() {
+				if (realSize < capacity) {
+					realSize++;
+					backPosition = realSize - 1;
+				}
+				else {
+					backPosition = (backPosition + 1) % capacity;
+				}
+			}
+			inline T& front() {
+				if (realSize < capacity)
+					return array[0];
+				else return array[(backPosition + 1) % capacity];
+			}
+			inline T& back() {
+				return array[backPosition];
+			}
+			inline T& operator[](size_t pos) {
+				return array[(((realSize < capacity ? 0 : (backPosition + 1) % capacity) + pos) % capacity + capacity) % capacity];
+			}
+			inline bool copy_back() {
+				emplace_back();
+				array[backPosition] = array[(backPosition - 1 + capacity) % capacity];
+				return true;
+			}
+			inline size_t size() {
+				return realSize;
+			}
+		};
 		RollArray<sf::Vector2f, scrollSensitivity>mousePos;
 		sf::Vector2f mouseLastPressPos;
 		sf::Vector2f mouseVelocity() {
